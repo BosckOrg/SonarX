4ebcc6cce70b97efa8a123ebb72036b4f444e34e
Teerapat (Bosck) <56102118+bxsck@users.noreply.github.com>
2024-09-12 17:28:47 +0700

    test: update test

--- client/src/tests/App.test.jsx
+++ client/src/tests/App.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 describe('App Component', () => {
   test('renders the component with initial elements', () => {
